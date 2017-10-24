f36cf8255953bafeb7d748057aadf83e74dcfa47
Danilo Lucas <danilolucasmd@gmail.com>
2017-10-24 13:42:43 -0200

    Add draft folder

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => (
+    <header className="main-header">
+        <div className="container">
+            <a className="header-logo">
+                <img />
+            </a>
+            <div className="header-content">
+            test
+            </div>
+        </div>
+    </header>
+);
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Header from './components/Header';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <div>
+        <Header />
+    </div>,
+    document.getElementById('root')
+);
