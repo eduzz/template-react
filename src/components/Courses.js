b136572d4d6304c7ac2e19702f6f4e9954e74300
Danilo Lucas <danilolucasmd@gmail.com>
2017-11-08 14:21:05 -0200

    Create generic component to active Filters menu with button

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -2,21 +2,17 @@ import React from 'react';
 import PageHeader from './PageHeader';
 import VisibleCourseCardGrid from '../containers/VisibleCourseCardGrid';
 import CoursesSearch from '../containers/CoursesSearch';
-import { FiltersMenu, ProducerFilter, PeriodFilter, StatusFilter, CategoryFilter, TagsFilter } from './Filters';
+import CoursesFilters from './CoursesFilters';
 
 const Courses = () => (
     <section className="page-content my-courses">
         <div className="container">
             <PageHeader />
-            <CoursesSearch />
+            <div className="search-bar">
+                <CoursesSearch />
+                <CoursesFilters />
+            </div>
             <VisibleCourseCardGrid />
-            <FiltersMenu>
-                <ProducerFilter active />
-                <PeriodFilter />
-                <StatusFilter active />
-                <CategoryFilter />
-                <TagsFilter />
-            </FiltersMenu>
         </div>
     </section>
 );
