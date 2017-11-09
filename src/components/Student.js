b93e54dc76eb341478d62469bcb5272222663f35
Will Dolmayan <willdolmayan@hotmail.com>
2017-11-09 17:21:06 -0200

    rotas criadas para tela de course details

--- src/components/Student.js
+++ src/components/Student.js
@@ -3,6 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import StudentCourses from './StudentCourses';
+import StudentCourseDetails from './StudentCourseDetails';
 
 const Student = () => (
 	<div className="template-black">
@@ -11,6 +12,7 @@ const Student = () => (
 		<Switch>
 			<Redirect exact from='/student' to='/student/courses' />
 	    	<Route exact path='/student/courses' component={ StudentCourses } />
+	    	<Route exact path='/student/courses/:courseID' component={ StudentCourseDetails } />
 	    </Switch>
 
 	    <Footer />
---
+++ src/components/StudentCourseDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StudentCourseDetails = () => (
+    <section className="student-content">
+        <div className="container">
+        	asdasd
+        </div>
+    </section>
+);
+
+export default StudentCourseDetails;
