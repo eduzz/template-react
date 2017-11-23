c6c60440be4d901cc99fd43516d819028c9cd465
Danilo Lucas <danilolucasmd@gmail.com>
2017-11-23 11:14:36 -0200

    Install material-ui in the application

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Route, Redirect, HashRouter, Switch } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Producer from 'components/Producer';
 import Student from 'components/Student';
 
 const Root = ({ store }) => (
-    <Provider store={store}>
-    	<HashRouter>
-	    	<div>
-		        <Switch>
-		        	<Redirect exact from='/' to='/producer' />
-		        	<Route path='/producer' component={ Producer } />
-		        	<Route path='/student' component={ Student } />
-		        </Switch>
-		    </div>
-		</HashRouter>
-  	</Provider>
+    <MuiThemeProvider>
+        <Provider store={store}>
+        	<HashRouter>
+    	    	<div>
+    		        <Switch>
+    		        	<Redirect exact from='/' to='/producer' />
+    		        	<Route path='/producer' component={ Producer } />
+    		        	<Route path='/student' component={ Student } />
+    		        </Switch>
+    		    </div>
+    		</HashRouter>
+      	</Provider>
+    </MuiThemeProvider>
 );
 
 export default Root;
