b6f3a15df9b54027e308f5ea45946a780335d262
Will Dolmayan <willdolmayan@hotmail.com>
2017-12-08 17:17:37 -0200

    header user menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,15 @@ const Header = () => (
             <a className="header-logo">
                 <img src="https://app.nutror.com/assets/img/super.png" alt="" />
             </a>
-            <div className="header-content"></div>
+            <nav className="user-menu" data-activates='dropdown1'>
+        		<div className="user-photo"><img src="http://static1.purepeople.com.br/articles/2/18/48/42/@/2157162-ashton-kutcher-criou-um-abaixo-assinado-237x237-2.jpg" alt=""/></div>
+            </nav>
+            <ul id='dropdown1' class='dropdown-content'>
+			    <li><a href="#!">Meus Dados</a></li>
+			    <li><a href="#!">Meus Certificados</a></li>
+			    <li class="divider"></li>
+			    <li><a href="#!">Sair</a></li>
+			</ul>
         </div>
     </header>
 );
