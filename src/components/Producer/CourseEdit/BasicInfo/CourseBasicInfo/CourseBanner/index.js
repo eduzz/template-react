d7c5c6e4a1e3372418dd9a1c1f423d3dcb60ab9d
Will Dolmayan <willdolmayan@hotmail.com>
2017-12-11 17:06:04 -0200

    tabs

--- src/components/Producer/CourseEdit/BasicInfo/CourseBasicInfo/CourseBanner/index.js
+++ src/components/Producer/CourseEdit/BasicInfo/CourseBasicInfo/CourseBanner/index.js
@@ -4,6 +4,7 @@ import styles from './styles.css';
 
 const CourseBanner = () => (
 	<section className={styles.component}>
+		<label className="input-label">Tamanho sugerido: 1920x400</label>
 		<div className="input-img large-banner">
 			<div className="container">
 				<a className="button small soft top-right">
