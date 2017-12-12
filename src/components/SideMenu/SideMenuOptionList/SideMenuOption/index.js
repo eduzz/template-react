6e3fb695f08dcd7df9c7b07acf35830036852c30
Jonathas Rodrigues <jonathasprodrigues@gmail.com>
2017-12-12 16:46:51 -0200

    Adicionado links no menu, configurado rotas e adicionado collapsible no menu

--- src/components/SideMenu/SideMenuOptionList/SideMenuOption/index.js
+++ src/components/SideMenu/SideMenuOptionList/SideMenuOption/index.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import Icon from 'components/Icon';
 import styles from './styles.css';
+import { Link } from 'react-router-dom';
 
-const SideMenuOption = ({ data, children }) => (
+function SideMenuOption ({ data, children }) {
+    if (children) {
+        return getCollapsible({data, children});
+    } else {
+        return getNormal({data});
+    }
+};
+
+const getNormal = ({data}) => (
     <li className={styles.component}>
-        <a href="" className="nav-button">
+        <Link to={data.to} className="nav-button">
             <Icon name={data.icon} />
             {data.title}
-        </a>
-        <ul className="submenu">
-            {children}
+        </Link>
+    </li>
+);
+
+const getCollapsible = ({data, children}) => (
+    <li className={styles.component}>
+        <ul className="collapsible" data-collapsible="accordion">
+            <li>
+                <div className="collapsible-header nav-button">
+                    <Icon name={data.icon} />
+                    {data.title}    
+                </div>
+                <ul className="collapsible-body submenu">
+                    {children}
+                </ul>
+            </li>
         </ul>
     </li>
 );
