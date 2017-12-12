e3c72c0ffe03fae4be9159713472f44aa89c19e3
Danilo Lucas <danilolucasmd@gmail.com>
2017-12-12 15:46:54 -0200

    Change router component to remove the # from the url

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Route, Redirect, HashRouter, Switch } from 'react-router-dom';
+import { Route, Redirect, BrowserRouter, Switch } from 'react-router-dom';
 import Producer from 'components/Producer';
 import Student from 'components/Student';
 import Login from 'components/Login';
@@ -9,14 +9,14 @@ import './root.css';
 
 const Root = ({ store }) => (
     <Provider store={store}>
-    	<HashRouter>
+    	<BrowserRouter>
 	        <Switch>
 	        	<Redirect exact from='/' to='/producer' />
                 <Route path='/login' component={ Login } />
 	        	<Route path='/producer' component={ Producer } />
 	        	<Route path='/student' component={ Student } />
 	        </Switch>
-		</HashRouter>
+		</BrowserRouter>
   	</Provider>
 );
 
