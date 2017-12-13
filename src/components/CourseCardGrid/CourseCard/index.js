e16a5f9d2fda3b552a85411aba880c94d2610cff
Jonathas Rodrigues <jonathasprodrigues@gmail.com>
2017-12-13 13:34:48 -0200

    Adiconado img no card

--- src/components/CourseCardGrid/CourseCard/index.js
+++ src/components/CourseCardGrid/CourseCard/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import styles from './styles.css';
+import { cdn } from '../../../constants';
 
 const CourseCard = ({ data }) => (
     <div className={styles.component}>
         <div className={`card-status free-course`}> GRATUITO </div>
+        <div className="card-image">
+            <img src={cdn+data.avatar} alt="..."/>
+        </div>
 
         <div className="card-description">
             <div className="card-title">{ data.title }</div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,2 @@
 export const api = 'http://nutror.devzz.ninja';
+export const cdn = 'https://app.nutror.com';
