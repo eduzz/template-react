ff3d8514b7b97c0b875fa5d1d255dfb53484e024
Will Dolmayan <willdolmayan@hotmail.com>
2017-12-14 10:35:33 -0200

    ajustes no float button e no header actions

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './styles.css';
+import Icon from 'components/Icon';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
     <header className={styles.component}>
@@ -7,15 +9,25 @@ const Header = () => (
             <a className="header-logo">
                 <img src="https://app.nutror.com/assets/img/super.png" alt="" />
             </a>
-            <nav className="user-menu" data-activates='dropdown1'>
-        		<div className="user-photo"><img src="http://static1.purepeople.com.br/articles/2/18/48/42/@/2157162-ashton-kutcher-criou-um-abaixo-assinado-237x237-2.jpg" alt=""/></div>
-            </nav>
-            <ul id='dropdown1' className='dropdown-content'>
-			    <li><a href="#!">Meus Dados</a></li>
-			    <li><a href="#!">Meus Certificados</a></li>
-			    <li className="divider"></li>
-			    <li><a href="#!">Sair</a></li>
-			</ul>
+            <div class="header-actions">
+                <Link to='/producer/courses'   className="button outline-light">
+                    <Icon name='students' />
+                    <span>Área do Produtor</span>
+                </Link>
+                <Link to='/student/courses'   className="button outline-light">
+                    <Icon name='library' />
+                    <span>Área de Ensino</span>
+                </Link>
+                 <nav className="user-menu" data-activates='dropdown1'>
+            		<div className="user-photo"><img src="http://static1.purepeople.com.br/articles/2/18/48/42/@/2157162-ashton-kutcher-criou-um-abaixo-assinado-237x237-2.jpg" alt=""/></div>
+                    <ul id='dropdown1' className='dropdown-content'>
+        			    <li><a href="#!">Meus Dados</a></li>
+        			    <li><a href="#!">Meus Certificados</a></li>
+        			    <li className="divider"></li>
+        			    <li><a href="#!">Sair</a></li>
+        			</ul>
+                </nav> 
+            </div>
         </div>
     </header>
 );
