c89ebe95b3701bff2f1232a88ea6dd2e697e00a9
Will Dolmayan <willdolmayan@hotmail.com>
2018-01-05 19:45:18 -0200

    Upsells grid

--- src/components/SideMenu/Welcome/index.js
+++ src/components/SideMenu/Welcome/index.js
@@ -5,7 +5,7 @@ const Welcome = ({ data }) => (
         <div className="user-photo"><img src="http://static1.purepeople.com.br/articles/2/18/48/42/@/2157162-ashton-kutcher-criou-um-abaixo-assinado-237x237-2.jpg" alt=""/></div>
         <div className="user-data">
             <span className="message">Bem vindo</span>
-            <h3 className="name">Brent Davis</h3>
+            <h3 className="name">Vinicius Nogueira</h3>
         </div>
     </div>
 );
