312408c42982d43f040ed2a07a03c005d14eb83e
Danilo Lucas <danilolucasmd@gmail.com>
2018-01-18 08:58:32 -0200

    Inplement input lifecycle on course edit page

--- src/reducers/course.js
+++ src/reducers/course.js
@@ -1,5 +1,7 @@
 const course = (state = {}, action) => {
     switch (action.type) {
+        case 'GET_COURSE':
+            return {};
         case 'RECEIVE_COURSE':
             return {...action.course};
         case 'RECEIVE_COURSE_ERROR':
