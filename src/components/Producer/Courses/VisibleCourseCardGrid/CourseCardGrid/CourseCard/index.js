c83aeb9444153884d91432f2c8b779344addb7ae
Danilo Lucas <danilolucasmd@gmail.com>
2018-02-07 17:04:42 -0200

    Change position of the link to open course edit

--- src/components/Producer/Courses/VisibleCourseCardGrid/CourseCardGrid/CourseCard/index.js
+++ src/components/Producer/Courses/VisibleCourseCardGrid/CourseCardGrid/CourseCard/index.js
@@ -4,17 +4,17 @@ import { cdn } from 'constants/index';
 import { Link } from 'react-router-dom';
 
 const CourseCard = ({ data }) => (
-    <div className={styles.component}>
+    <Link to={`courses/${data.id}`} className={styles.component}>
         <div className={`card-status free-course`}> GRATUITO </div>
         <div className="card-image">
             <img src={cdn+data.avatar} alt="..."/>
         </div>
 
-        <Link to={`courses/${data.id}`} className="card-description">
+        <div className="card-description">
             <p className="card-title">{ data.title }</p>
             <p className="card-category">{ data.category.name }</p>
-        </Link>
-    </div>
+        </div>
+    </Link>
 );
 
 export default CourseCard;
