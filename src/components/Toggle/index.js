9c67e53882e65040a894f2e0e92ac8ec77e4dc72
Michel Makei Gefuni <michel.gefuni@eduzz.com>
2018-02-21 18:16:35 -0300

    Toogle component - added de correct component and custom style

--- src/components/Toggle/index.js
+++ src/components/Toggle/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Toggle = (props) => (
-            <div className="switch">
-                <label>
-                <input type="checkbox" id="check-destaque" onClick={props.onClick}/>
-                <span className="lever"></span>
-                </label>
-                <label htmlFor="check-destaque">
-                <h3 className="form-block-title">{props.title}</h3>
-                <p className="check-description">{props.description}</p>
-                </label>
-            </div>
+    <div className="switch">
+        <label>
+            <input type="checkbox" id="check-destaque" onClick={props.onClick}/>
+            <span className="lever"></span>
+        </label>
+        <label htmlFor="check-destaque">
+            <h3 className="form-block-title">{props.title}</h3>
+            <p className="check-description">{props.description}</p>
+        </label>
+    </div>
 );
 
 Toggle.propTypes = {
