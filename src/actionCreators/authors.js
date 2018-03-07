6109bb7392175f5d1480d9a497a269c8236d95e3
danilolucasmd <danilolucasmd@gmail.com>
2018-03-07 09:50:18 -0300

    Add author action creators

--- src/actionCreators/authors.js
+++ src/actionCreators/authors.js
@@ -20,15 +20,15 @@ export const fetchAuthors = () => dispatch =>
     err => dispatch(receiveAuthorsError(err))
   );
 
-// const receiveAuthor = author => ({
-//   type: 'RECEIVE_AUTHOR',
-//   author
-// });
+const receiveAuthor = author => ({
+  type: 'RECEIVE_AUTHOR',
+  author
+});
 
-// const receiveAuthorError = err => ({
-//   type: 'RECEIVE_AUTHOR_ERROR',
-//   err
-// });
+const receiveAuthorError = err => ({
+  type: 'RECEIVE_AUTHOR_ERROR',
+  err
+});
 
 export const addAuthor = name => ({
   type: 'ADD_AUTHOR',
