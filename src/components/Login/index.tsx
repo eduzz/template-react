5ba4d5619041bd9f9ba27a272e4917f03af9d2a5
Michel Makei Gefuni <michelgefuni@gmail.com>
2018-04-20 10:18:15 -0300

    Login page
    Criadas as rotas de componentes

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,31 +1,37 @@
-import React, { Component } from 'react';
-import SignIn from './signin';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Footer from 'components/Footer';
 import NutrorLogo from 'components/NutrorLogo';
+import SignIn from './signin';
+import SignUp from './signup';
 
 const styles = require('./styles.css');
 const loginbg = require('assets/img/login-bg.jpg');
 
-export default class Login extends Component {
-  render() {
-    return (
-      <div className={styles.component}>
-        <div className='login-form'>
-          <div className='card-panel'>
-            <div className='default-logo'>
-              <div className='nutror-logo'>
-                <NutrorLogo />
-              </div>
+const producer = () => (
+  <div className={styles.component}>
+    <div className='login-form'>
+      <div>
+        <div className='card-panel'>
+          <div className='default-logo'>
+            <div className='nutror-logo'>
+              <NutrorLogo />
             </div>
           </div>
-          <SignIn />
-          <Footer />
         </div>
 
-        <div className='login-background'>
-          <img src={loginbg} alt='' />
-        </div>
+        <Switch>
+          <Route exact path='/login' component={SignIn} />
+          <Route exact path='/login/signup' component={SignUp} />
+        </Switch>
       </div>
-    );
-  }
-}
+      <Footer />
+    </div>
+
+    <div className='login-background'>
+      <img src={loginbg} alt='' />
+    </div>
+  </div>
+);
+
+export default producer;
