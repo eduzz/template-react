0e24ed79002eea6525d5dae63e35fb9441e31ddb
Daniel Prado <danielprado.ad@gmail.com>
2018-05-23 17:49:51 -0300

    fix docker build

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -5,12 +5,12 @@ cp ./docker/prod/.dockerignore .dockerignore
 echo 'Generating docker...'
 if [ "$1" = '--production' ] || [ "$1" = '-p' ]; then
   echo 'BUILDING FOR PRODUCTION...'
-  docker build -t eduzz/nutror-v3-front:producer -f docker/prod/Dockerfile .
-  # docker push eduzz/nutror-v3-front:producer
+  docker build -t infraeduzz/nutror-front:producer -f docker/prod/Dockerfile .
+  # docker push infraeduzz/nutror-front:producer
 else
   echo 'BUILDING FOR HOMOLOG...'
-  docker build -t eduzz/nutror-v3-front:producer-hml -f docker/prod/Dockerfile .
-  # docker push eduzz/nutror-v3-front:producer-hml
+  docker build -t infraeduzz/nutror-front:producer-hml -f docker/prod/Dockerfile .
+  # docker push infraeduzz/nutror-front:producer-hml
 fi
 
 rm .dockerignore
