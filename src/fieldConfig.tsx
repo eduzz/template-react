f9b0ef1abe1bffbc50db361abb0c5e819793fe50
Daniel Prado <danielprado.ad@gmail.com>
2018-06-04 15:24:40 -0300

    remove console.log

--- src/fieldConfig.tsx
+++ src/fieldConfig.tsx
@@ -1,9 +1,6 @@
 import { setConfig } from 'material-ui-form-fields/dist/config';
 import commonMasks from 'material-ui-form-fields/dist/mask/common/pt-br';
-import { validate } from 'material-ui-form-fields/dist/validator';
 import validationMessage from 'material-ui-form-fields/dist/validator/custom-languages/pt-br';
-
-console.log(validate);
 
 setConfig({
   masks: commonMasks,
