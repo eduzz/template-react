9a35648c8d4b004441eba0046647a922dbb5be06
Will Dolmayan <willdolmayan@hotmail.com>
2018-06-15 18:14:20 -0300

    Adicionado Resumo dos Alunos

--- src/assets/theme.tsx
+++ src/assets/theme.tsx
@@ -7,7 +7,11 @@ const variables = {
   tabbarHeight: 48,
   contentPadding: 12,
   contentPaddingUpSm: 24,
-  boxShadow: '0px 3px 5px -1px rgba(0, 0, 0, 0.2), 0px 5px 8px 0px rgba(0, 0, 0, 0.14), 0px 1px 14px 0px rgba(0, 0, 0, 0.12)'
+  boxShadow: '0px 3px 5px -1px rgba(0, 0, 0, 0.2), 0px 5px 8px 0px rgba(0, 0, 0, 0.14), 0px 1px 14px 0px rgba(0, 0, 0, 0.12)',
+  color: {
+    sucess: '#009358',
+    error: '#dc3f53',
+  }
 };
 
 const primary = {
--- src/components/Pages/Student/List/index.tsx
+++ src/components/Pages/Student/List/index.tsx
@@ -1,25 +1,96 @@
-import React, { PureComponent, Fragment } from 'react';
+import {
+  Button,
+  Card,
+  CardContent,
+  Chip,
+  Collapse,
+  Grid,
+  Tab,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Tabs,
+  Typography,
+} from '@material-ui/core';
+import DropdownMenu from 'components/DropdownMenu';
 import Toolbar from 'components/Toolbar';
-import { Grid, Tab, Tabs, Card, Table, TableBody, TableCell, TableHead, TableRow, Button, CardContent } from '@material-ui/core';
 import { WithStyles } from 'decorators/withStyles';
+import { FieldSelect } from 'material-ui-form-fields';
+import { AccountAlertIcon, AccountCheckIcon, AccountMultipleIcon, AccountMultiplePlusIcon } from 'mdi-react';
+import ExitToAppIcon from 'mdi-react/ExitToAppIcon';
+import KeyVariantIcon from 'mdi-react/KeyVariantIcon';
+import React, { Fragment, PureComponent } from 'react';
+
 interface IState {
   currentTab: number;
   data: any[];
+  chipContent: any[];
+  event: any[];
+  isFilterOpen: boolean;
 }
 
 interface IProps {
   classes?: any;
 }
 
-@WithStyles({
-  tableFloatButton: {
-    float: 'right'
+@WithStyles(theme => ({
+  rightFloatButton: {
+    float: 'right',
+  },
+  filter: {
+    backgroundColor: '#fafafa',
+    borderBottom: '1px solid #cecece',
+  },
+  filterContent: {
+    padding: '8px 16px',
+  },
+  filterTags: {
+    backgroundColor: '#fff',
+    border: '1px dashed #cecece',
+    marginTop: 24,
+  },
+  chip: {
+    marginRight: theme.spacing.unit / 2,
+  },
+  cardTitle: {
+    fontSize: '0.85rem',
+    fontWeight: 'bold',
+  },
+  cardData: {
+    fontSize: '2rem',
+    textAlign: 'right',
+    color: theme.palette.primary.dark,
+  },
+  cardDataNegative: {
+    color: theme.variables.color.error,
+  },
+  cardDataPositive: {
+    color: theme.variables.color.sucess,
+  },
+  cardIcon: {
+    fill: theme.palette.primary.dark,
+    marginRight: theme.spacing.unit,
+    height: '2rem',
+    width: '2rem',
+  },
+  cardIconNegative: {
+    fill: theme.variables.color.error,
+  },
+  cardIconPositive: {
+    fill: theme.variables.color.sucess,
+  },
+  studentsResume: {
+    marginBottom: theme.spacing.unit * 3,
+    marginTop: theme.spacing.unit * 3,
   }
-})
+}))
+
 export default class StudentListPage extends PureComponent<IProps, IState> {
   constructor(props: any) {
     super(props);
-    this.state = { currentTab: 0, data: [] };
+    this.state = { currentTab: 0, data: [], chipContent: [], event: [], isFilterOpen: false };
   }
 
   componentDidMount() {
@@ -36,14 +107,31 @@ export default class StudentListPage extends PureComponent<IProps, IState> {
       createData(789789, 'Michel Makei', '12/12/23', '67%', 43),
       createData(98, 'Henrique Gomes', '12/12/23', '49%', 39),
     ];
+    const chipContent = [
+      { key: 0, label: 'Curso' },
+      { key: 1, label: 'Data de Inicio' },
+      { key: 2, label: 'Progresso' },
+      { key: 3, label: 'Status:Inativo' },
+      { key: 4, label: 'Matricula: 2344' }
+    ];
 
-    this.setState({ data });
+    this.setState({ data, chipContent });
   }
 
   onTabChange(event: any, tab: number) {
     this.setState({ currentTab: tab });
   }
 
+  handleDelete = (data: any) => () => {
+    const chipContent = this.state.chipContent.filter(c => c != data);
+    this.setState({ chipContent });
+  }
+  handleToggleFilter = (e: any) => {
+    e.preventDefault();
+    this.setState({
+      isFilterOpen: !this.state.isFilterOpen,
+    });
+  }
   render() {
     const { currentTab, data } = this.state;
     const { classes } = this.props;
@@ -51,26 +139,128 @@ export default class StudentListPage extends PureComponent<IProps, IState> {
     return (
       <Fragment>
         <Toolbar title='Gerenciamento de Alunos' />
-        <Card>
-          <CardContent>
-            <p>Filtro Esperto tchananam</p>
-
-          </CardContent>
-        </Card>
+        <Grid container spacing={16} className={classes.studentsResume}>
+          <Grid item xs={3}>
+            <Card>
+              <CardContent>
+                <Typography className={`${classes.cardTitle} ${classes.cardDataPositive}`}>Novos Alunos nos ultimos 30d</Typography>
+                <Typography className={`${classes.cardData} ${classes.cardDataPositive}`}>
+                  <AccountMultiplePlusIcon className={`${classes.cardIcon} ${classes.cardIconPositive}`} />187</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={3}>
+            <Card>
+              <CardContent>
+                <Typography className={classes.cardTitle}>Alunos Ativos</Typography>
+                <Typography className={classes.cardData}> <AccountCheckIcon className={classes.cardIcon} />23554</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={3}>
+            <Card>
+              <CardContent>
+                <Typography className={`${classes.cardTitle} ${classes.cardDataNegative}`}>Alunos Inativos</Typography>
+                <Typography className={`${classes.cardData} ${classes.cardDataNegative}`}>
+                  <AccountAlertIcon className={`${classes.cardIcon} ${classes.cardIconNegative}`} />
+                  322
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={3}>
+            <Card>
+              <CardContent>
+                <Typography className={classes.cardTitle}>Total de Alunos</Typography>
+                <Typography className={classes.cardData}> <AccountMultipleIcon className={classes.cardIcon} />21345</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
         <Card>
           <Grid container spacing={16}>
-            <Grid item xs={10}>
+            <Grid item xs={6} sm={8} md={10}>
               <Tabs value={currentTab} onChange={this.onTabChange.bind(this)}>
                 <Tab label='Todos' />
                 <Tab label='Ativos' />
               </Tabs>
             </Grid>
-            <Grid item xs={2}>
+            <Grid item xs={3} sm={2} md={1}>
+              <Button
+                color='primary'
+                size='medium'
+                onClick={this.handleToggleFilter}
+                className={`${classes.rightFloatButton} ${this.state.isFilterOpen ? 'active' : ''}`}>Filtrar
+                </Button>
+            </Grid>
+            <Grid item xs={3} sm={2} md={1}>
               <Button
                 color='secondary'
                 size='medium'
-                className={classes.tableFloatButton}>Exportar
+                className={classes.rightFloatButton}>Exportar
                 </Button>
+            </Grid>
+          </Grid>
+          <Grid container>
+            <Grid item xs={12}>
+              <Collapse className={classes.filter} in={this.state.isFilterOpen}>
+                <CardContent className={classes.filterContent}>
+                  <Grid container spacing={16} alignItems='center'>
+                    <Grid item xs={4}>
+                      <FieldSelect
+                        label='Filtrar por:'
+                        options={[
+                          { value: 1, label: 'Cursos' },
+                          { value: 2, label: 'Nome' },
+                          { value: 3, label: 'Matricula' }
+                        ]}
+                        value={1}
+                        onChange={() => null}
+                      />
+                    </Grid>
+                    <Grid item xs={4}>
+                      <FieldSelect
+                        label='Selecione o Curso'
+                        options={[
+                          { value: 1, label: 'Marketex - Como criar videos profissionais' },
+                          { value: 2, label: 'Curso de Photoshop' },
+                          { value: 3, label: 'React RxJS' }
+                        ]}
+                        value={1}
+                        onChange={() => null}
+                      />
+                    </Grid>
+                    <Grid item xs={4}>
+                      <Button
+                        color='secondary'
+                        variant='raised'
+                        size='medium'>Adicionar Filtro
+                      </Button>
+                    </Grid>
+                  </Grid>
+                  <Grid container spacing={16} >
+                    <Grid item xs={12} className={classes.filterTags}>
+                      {this.state.chipContent.map(data => {
+
+                        return (
+                          <Chip
+                            key={data.key}
+                            label={data.label}
+                            onDelete={this.handleDelete(data)}
+                            className={classes.chip}
+                          />
+                        );
+                      })}
+                      <Button
+                        color='secondary'
+                        className={classes.rightFloatButton}>
+                        Limpar Filtros
+                        </Button>
+                    </Grid>
+                  </Grid>
+                </CardContent>
+              </Collapse>
+
             </Grid>
           </Grid>
           <Table className={classes.table}>
@@ -93,7 +283,17 @@ export default class StudentListPage extends PureComponent<IProps, IState> {
                     <TableCell>{n.name}</TableCell>
                     <TableCell>{n.beginDate}</TableCell>
                     <TableCell>{n.progress}</TableCell>
-                    <TableCell numeric>{n.actions}</TableCell>
+                    <TableCell numeric>
+                      <DropdownMenu options={[{
+                        text: 'Resetar a Senha',
+                        icon: KeyVariantIcon,
+                        handler: () => { }
+                      }, {
+                        text: 'Editar Aluno',
+                        icon: ExitToAppIcon,
+                        handler: () => { }
+                      }]} />
+                    </TableCell>
                   </TableRow>
                 );
               })}
