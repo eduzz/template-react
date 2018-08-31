b127fe92ecb9a765ecc6718d9477c882964c17df
Michel Makei Gefuni <michelgefuni@gmail.com>
2018-08-31 15:29:10 -0300

    Certificate -> Order
    Criados os botões para ordenar a lista

--- src/components/Pages/Admin/Certificate/List/index.tsx
+++ src/components/Pages/Admin/Certificate/List/index.tsx
@@ -1,13 +1,20 @@
-import { CardContent, CircularProgress } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import FieldSelect from '@react-form-fields/material-ui/components/Select';
 import Toolbar from 'components/Layout/Toolbar';
 import AppRouter, { RouterContext } from 'components/Router';
 import ErrorMessage from 'components/Shared/ErrorMessage';
 import FabButton from 'components/Shared/FabButton';
 import { WithStyles } from 'decorators/withStyles';
 import { ICertificate } from 'interfaces/models/certificate';
+import ArrowDownIcon from 'mdi-react/ArrowDownIcon';
+import ArrowUpIcon from 'mdi-react/ArrowUpIcon';
 import PlusIcon from 'mdi-react/PlusIcon';
+import SortVariantIcon from 'mdi-react/SortVariantIcon';
 import React, { Fragment, PureComponent } from 'react';
 import rxjsOperators from 'rxjs-operators';
 import certificateService from 'services/certificate';
@@ -17,6 +24,8 @@ import CertificateItem from './ListItem';
 interface IState {
   error?: any;
   certificates?: ICertificate[];
+  orderBy: string;
+  orderDirection: 'asc' | 'desc';
 }
 
 interface IProps {
@@ -26,9 +35,6 @@ interface IProps {
 
 @WithStyles(theme => ({
   title: {
-    ...theme.mixins.gutters(),
-    paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2,
   },
   loader: {
     textAlign: 'center'
@@ -42,7 +48,10 @@ class CertificateListPage extends PureComponent<IProps, IState> {
 
   constructor(props: IProps) {
     super(props);
-    this.state = {};
+    this.state = {
+      orderBy: 'title',
+      orderDirection: 'asc'
+    };
   }
 
   componentDidMount() {
@@ -51,8 +60,9 @@ class CertificateListPage extends PureComponent<IProps, IState> {
 
   loadData = () => {
     this.setState({ error: null, certificates: null });
+    const { orderBy, orderDirection } = this.state;
 
-    certificateService.list().pipe(
+    certificateService.list(orderBy, orderDirection).pipe(
       rxjsOperators.delay(1000),
       rxjsOperators.logError(),
       rxjsOperators.bindComponent(this),
@@ -61,9 +71,17 @@ class CertificateListPage extends PureComponent<IProps, IState> {
     }, error => this.setState({ error }));
   }
 
+  handleChangeOrderBy = (orderBy: string) => {
+    this.setState({ orderBy }, () => this.loadData());
+  }
+
+  toggleOrderDirection = () => {
+    this.setState({ orderDirection: this.state.orderDirection === 'asc' ? 'desc' : 'asc' }, () => this.loadData());
+  }
+
   render() {
     const { classes } = this.props;
-    const { certificates, error } = this.state;
+    const { certificates, error, orderBy, orderDirection } = this.state;
 
     return (
       <Fragment>
@@ -72,9 +90,37 @@ class CertificateListPage extends PureComponent<IProps, IState> {
         <FabButton actions={this.actions} />
 
         <Card>
-          <Typography className={classes.title} variant='subheading'>
-            Cursos que foram atribuídos ao certificado
-        </Typography>
+          <CardContent>
+            <Grid container spacing={16} alignItems='center'>
+              <Grid item xs={true}>
+                <Typography className={classes.title} variant='subheading'>
+                  Cursos que foram atribuídos ao certificado
+                </Typography>
+              </Grid>
+
+              <Grid item xs={false}>
+                <FieldSelect
+                  value={orderBy}
+                  options={[{ value: 'title', label: 'Título' }, { value: 'created_at', label: 'Data de criação' }]}
+                  onChange={this.handleChangeOrderBy}
+                  fullWidth={false}
+                  disabled={!error && !certificates}
+                  margin='none'
+                />
+              </Grid>
+
+              <Grid item xs={false}>
+                <IconButton
+                  disabled={!error && !certificates}
+                  onClick={this.toggleOrderDirection}
+                >
+                  {orderDirection === 'asc' ? <ArrowDownIcon /> : <ArrowUpIcon />}
+                  <SortVariantIcon />
+                </IconButton>
+              </Grid>
+
+            </Grid>
+          </CardContent>
 
           {!error && !certificates &&
             <CardContent className={classes.loader}>
@@ -88,9 +134,9 @@ class CertificateListPage extends PureComponent<IProps, IState> {
             </CardContent>
           }
 
-          {!!certificates && certificates.map(certificate =>
+          {!!certificates && certificates.map(certificate => (
             <CertificateItem key={certificate.id} certificate={certificate} />
-          )}
+          ))}
         </Card>
       </Fragment>
     );
