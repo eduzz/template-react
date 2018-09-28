b5a95ac167766d3358d49c91181ae0e9547bb430
Daniel Prado <danielprado.ad@gmail.com>
2018-09-28 11:18:51 -0300

    Login: permite apenas produtor

--- src/interfaces/userToken.ts
+++ src/interfaces/userToken.ts
@@ -3,7 +3,13 @@ export interface IUserToken {
   email: string;
   name: string;
   exp: number;
+  type: enUserType;
 
   firstName: string;
   canAccess(...roles: string[]): boolean;
+}
+
+export enum enUserType {
+  PRODUCER = 1,
+  LEARNER = 2
 }
