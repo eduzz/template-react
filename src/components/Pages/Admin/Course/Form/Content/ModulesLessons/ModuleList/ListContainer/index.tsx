16c6c990a7efed15f940c9f467ec8f94395d92de
Danilo <danilolucasmd@Danilos-iMac.local>
2018-11-05 14:07:24 -0200

    Implement new module form

--- src/components/Pages/Admin/Course/Form/Content/ModulesLessons/ModuleList/ListContainer/index.tsx
+++ src/components/Pages/Admin/Course/Form/Content/ModulesLessons/ModuleList/ListContainer/index.tsx
@@ -4,16 +4,22 @@ import List from '@material-ui/core/List';
 import { IModule } from 'interfaces/models/module';
 import ModuleItem from './ModuleItem';
 import { WithStyles } from 'decorators/withStyles';
+import ListItem from '@material-ui/core/ListItem';
+import AddForm from './AddForm';
 
 interface IProps {
   modules: IModule[];
   classes?: any;
   onLessonSort: Function;
+  onAddModule: Function;
 }
 
 @WithStyles({
   root: {
-    // padding: '0 16px 16px 16px',
+  },
+  addForm: {
+    borderTop: 'solid 1px #d5d5d5',
+    paddingTop: 20,
   },
 })
 class ListContainer extends PureComponent<IProps> {
@@ -30,6 +36,11 @@ class ListContainer extends PureComponent<IProps> {
             onLessonSort={onLessonSort}
           />
         ))}
+        <ListItem className={classes.addForm}>
+          <AddForm
+            onAdd={this.props.onAddModule}
+          />
+        </ListItem>
       </List>
     );
   }
