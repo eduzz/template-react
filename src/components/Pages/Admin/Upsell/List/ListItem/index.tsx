91ad72df0169e4ac1bf529718a6b858cc9ae1313
m_gefuni <michel.gefuni@eduzz.com>
2018-12-11 15:36:35 -0200

    Minhas ofertas -> List
    Revisada a exibição no mobile

--- src/components/Pages/Admin/Upsell/List/ListItem/index.tsx
+++ src/components/Pages/Admin/Upsell/List/ListItem/index.tsx
@@ -1,4 +1,5 @@
 import Grid from '@material-ui/core/Grid';
+import Hidden from '@material-ui/core/Hidden';
 import ListItem from '@material-ui/core/ListItem';
 import Typography from '@material-ui/core/Typography';
 import AppRouter, { RouterContext } from 'components/Router';
@@ -7,17 +8,17 @@ import DropdownMenu from 'components/Shared/DropdownMenu';
 import Toast from 'components/Shared/Toast';
 import { WithStyles } from 'decorators/withStyles';
 import { IUpsellList } from 'interfaces/models/upsell';
+import CursorDefaultIcon from 'mdi-react/CursorDefaultIcon';
+import EyeIcon from 'mdi-react/EyeIcon';
 import SquareEditOutlineIcon from 'mdi-react/SquareEditOutlineIcon';
 import TrashCanIcon from 'mdi-react/TrashCanIcon';
 import React, { PureComponent, SyntheticEvent } from 'react';
 import rxjsOperators from 'rxjs-operators';
 import upsellService from 'services/upsell';
-import EyeIcon from 'mdi-react/EyeIcon';
-import CursorDefaultIcon from 'mdi-react/CursorDefaultIcon';
-// import ChartPieIcon from 'mdi-react/ChartPieIcon';
-// import ArrowUpIcon from 'mdi-react/ArrowUpIcon';
 import { CDN_URL } from 'settings';
 
+// import ChartPieIcon from 'mdi-react/ChartPieIcon';
+// import ArrowUpIcon from 'mdi-react/ArrowUpIcon';
 const nutrorLogo = require('assets/svg/nutror-logo.svg');
 
 interface IProps {
@@ -91,28 +92,32 @@ class UpsellItem extends PureComponent<IProps> {
 
     return (
       <ListItem className={classes.root}>
-        <Grid container spacing={16} alignItems='center'>
-          <Grid item>
-            <Grid container>
-              <img
-                alt=''
-                className={classes.avatar}
-                src={CDN_URL + upsell.small_image}
-                onError={this.handleImageError}
-                height={44}
-              />
+        <Grid container spacing={16} justify='flex-end' alignItems='center'>
+          <Hidden xsDown>
+            <Grid item>
+              <Grid container>
+                <img
+                  alt=''
+                  className={classes.avatar}
+                  src={CDN_URL + upsell.small_image}
+                  onError={this.handleImageError}
+                  height={44}
+                />
+              </Grid>
             </Grid>
-          </Grid>
 
-          <Grid item xs={4}>
-            <Typography variant='subtitle2' className={classes.title} noWrap>{upsell.title}</Typography>
-          </Grid>
+            <Grid item xs={4}>
+              <Typography variant='subtitle2' className={classes.title} noWrap>{upsell.title}</Typography>
+            </Grid>
+          </Hidden>
 
-          <Grid item xs={true}>
-            {/* <Typography variant='subtitle1' noWrap>
-              R$ {this.randomizeInt(999)},{this.randomizeInt(9)}{this.randomizeInt(9)}
-            </Typography> */}
-          </Grid>
+          <Hidden smDown>
+            <Grid item xs={true}>
+              {/* <Typography variant='subtitle1' noWrap>
+                R$ {this.randomizeInt(999)},{this.randomizeInt(9)}{this.randomizeInt(9)}
+              </Typography> */}
+            </Grid>
+          </Hidden>
 
           <Grid item xs={false}>
             <Grid container alignItems='center'>
@@ -166,9 +171,19 @@ class UpsellItem extends PureComponent<IProps> {
             </Grid>
           </Grid> */}
 
+          <Hidden smUp>
+            <Grid item xs={true} />
+          </Hidden>
+
           <Grid item xs={false}>
             <DropdownMenu options={this.actions} />
           </Grid>
+
+          <Hidden smUp>
+            <Grid item xs={12}>
+              <Typography variant='subtitle2' className={classes.title} noWrap>{upsell.title}</Typography>
+            </Grid>
+          </Hidden>
         </Grid>
       </ListItem>
     );
