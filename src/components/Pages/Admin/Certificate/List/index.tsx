9f8d3f643b636673c77648233e71cf7eaec8fd2b
m_gefuni <michel.gefuni@eduzz.com>
2018-12-12 15:42:59 -0200

    Certificados -> List
    Revisados os ajustes responsivos

--- src/components/Pages/Admin/Certificate/List/index.tsx
+++ src/components/Pages/Admin/Certificate/List/index.tsx
@@ -3,9 +3,11 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from '@material-ui/core/Grid';
+import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import FieldSelect from '@react-form-fields/material-ui/components/Select';
+import { theme } from 'assets/theme';
 import Toolbar from 'components/Layout/Toolbar';
 import AppRouter, { RouterContext } from 'components/Router';
 import ErrorMessage from 'components/Shared/ErrorMessage';
@@ -40,6 +42,11 @@ interface IProps {
   card: {
     minHeight: 'fit-content',
   },
+  orderSelect: {
+    [theme.breakpoints.only('xs')]: {
+      width: '100%',
+    },
+  }
 })
 class CertificateListPage extends PureComponent<IProps, IState> {
   constructor(props: IProps) {
@@ -86,11 +93,17 @@ class CertificateListPage extends PureComponent<IProps, IState> {
         <Toolbar>
           <Grid container spacing={16} alignItems='center'>
             <Grid item xs={true}>
-              <Typography variant='h6' noWrap>Certificados</Typography>
+              <Typography variant='h6' noWrap>
+                <Hidden xsDown>Certificados</Hidden>
+                <Hidden smUp><small>Certificado</small></Hidden>
+              </Typography>
             </Grid>
 
             <Grid item xs={false}>
-              <Button variant='contained' color='secondary' onClick={this.handleNew}><PlusIcon /> Criar novo certificado</Button>
+              <Button size='small' variant='contained' color='secondary' onClick={this.handleNew}><PlusIcon />
+                <Hidden xsDown>Criar novo certificado</Hidden>
+                <Hidden smUp>Criar novo</Hidden>
+              </Button>
             </Grid>
           </Grid>
         </Toolbar>
@@ -98,11 +111,11 @@ class CertificateListPage extends PureComponent<IProps, IState> {
         <Card>
           <CardContent>
             <Grid container spacing={16} alignItems='center'>
-              <Grid item xs={true}>
+              <Grid item xs={window.innerWidth > theme.breakpoints.values.sm ? true : 12}>
                 <Typography variant='subtitle1'>Cursos que foram atribuídos ao certificado</Typography>
               </Grid>
 
-              <Grid item xs={false}>
+              <Grid item xs={window.innerWidth > theme.breakpoints.values.sm ? false : 8}>
                 <FieldSelect
                   value={orderBy}
                   options={[{ value: 'title', label: 'Título' }, { value: 'created_at', label: 'Data de criação' }]}
@@ -110,10 +123,11 @@ class CertificateListPage extends PureComponent<IProps, IState> {
                   fullWidth={false}
                   disabled={!error && !certificates}
                   margin='none'
+                  className={classes.orderSelect}
                 />
               </Grid>
 
-              <Grid item xs={false}>
+              <Grid item xs={window.innerWidth > theme.breakpoints.values.sm ? false : 4}>
                 <IconButton
                   disabled={!error && !certificates}
                   onClick={this.toggleOrderDirection}
