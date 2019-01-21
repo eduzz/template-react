03a85cd8713805d4f8854a9a1e66dfd23e537f3e
danieloprado <danielprado.ad@gmail.com>
2019-01-21 16:17:57 -0200

    update
    Signed-off-by: danieloprado <danielprado.ad@gmail.com>

--- src/settings.tsx
+++ src/settings.tsx
@@ -1,6 +1,6 @@
 export const ENV = (process.env.REACT_APP_ENV || 'production').trim();
 
-export const BRANCH_NAME = (process.env.REACT_APP_BRANCH_NAME || 'TESTE').trim();
+export const BRANCH_NAME = (process.env.REACT_APP_BRANCH_NAME || '').trim();
 
 export const API_ENDPOINT = (process.env.REACT_APP_API_ENDPOINT || '').trim();
 if (!API_ENDPOINT) throw new Error('Please provide an API_ENDPOINT');
