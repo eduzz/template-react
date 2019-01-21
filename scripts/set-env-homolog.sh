1f94fbb8cbbfe621335b4288fac9f3031cb3e3c5
Diogo M Mainardes <diogo.mainardes@eduzz.com>
2019-01-21 14:49:12 -0200

    Acrescentando deploy em QA das hotfix e verificação se é QA

--- scripts/set-env-homolog.sh
+++ scripts/set-env-homolog.sh
@@ -1,3 +1,10 @@
 cat .env.homolog > .env.production.local
+
+echo "
+REACT_APP_BRANCH_NAME=$1
+REACT_APP_BUILD_NUMBER=$2
+REACT_APP_BUILD_DATE=\"$(date +"%Y-%m-%d %H:%M:%S")\"
+" >> .env.production.local
+
 echo "Variáveis de homologação definidas"
 cat .env.production.local
