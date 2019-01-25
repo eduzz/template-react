5874970ef875ddf50a7138cabe9f6579a5bd54cf
danieloprado <danielprado.ad@gmail.com>
2019-01-25 10:27:56 -0200

    fix date safari
    Signed-off-by: danieloprado <danielprado.ad@gmail.com>

--- src/formatters/date.ts
+++ src/formatters/date.ts
@@ -3,11 +3,14 @@ import dateFnsIsValid from 'date-fns/isValid';
 import locale from 'date-fns/locale/pt-BR';
 import dateFnsParse from 'date-fns/parse';
 
-export function dateParse(value: any, format: string = null): Date {
+export function dateParse(value: any, format: string = 'yyyy-MM-dd HH:mm:ss'): Date {
   if (!value) return value;
-  if (value instanceof Date) return value;
+  if (typeof value !== 'string') return value;
 
+  value = value.replace(/\+.+/gi, '').replace(/\..+$/gi, '');
   const date = !format ? new Date(value) : dateFnsParse(value, format, new Date(), { locale });
+  (window as any).f = (value: string, format: string) => dateFnsParse(value, format, new Date(), { locale });
+  console.log(date, value, format);
   if (!dateFnsIsValid(date)) return value;
 
   return date;
