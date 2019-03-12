3fc0fe85a2ba1097fe3541442da29079142a54ba
m_gefuni <michel.gefuni@eduzz.com>
2019-03-12 16:03:40 -0300

    StudentList -> SendEmail
    Ajuste no formato de envio da mensagem + troca de componente

--- src/fieldConfig.tsx
+++ src/fieldConfig.tsx
@@ -3,6 +3,18 @@ import lang from '@react-form-fields/material-ui/lang/pt-br';
 
 const fieldConfig = new ConfigBuilder()
   .fromLang(lang)
+  .setEditorConfig('pt', {
+    inline: { options: ['bold', 'italic', 'underline'] },
+    list: { options: ['unordered', 'ordered'] },
+    embedded: { className: 'hide' },
+    fontSize: { className: 'hide' },
+    blockType: { className: 'hide' },
+    remove: { className: 'hide' },
+    colorPicker: { className: 'hide' },
+    fontFamily: { className: 'hide' },
+    textAlign: { className: 'hide' },
+    emoji: { className: 'hide' }
+  })
   .setValidationOn('onSubmit')
   .build();
 
