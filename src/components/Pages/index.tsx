4dd2db2c3c4b31f936bffe92ed9ff558a5f8a55f
Fernando Moraes <fernando.moraes@eduzz.com>
2020-07-22 15:43:49 -0300

    Revert "version 13"
    This reverts commit dd544710478fd1e568450145103188fbb5940417.

--- src/components/Pages/index.tsx
+++ src/components/Pages/index.tsx
@@ -1,3 +1,4 @@
+import PermissionRoute from 'components/Shared/PermissionRoute';
 import React, { memo, useCallback } from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
@@ -12,7 +13,7 @@ const Pages = memo(() => {
     <BrowserRouter>
       <Switch>
         <Route path='/nova-senha' exact component={NewPasswordPage} />
-        <Route path='/' component={AdminPage} />
+        <PermissionRoute path='/' component={AdminPage} />
 
         <Route path='/reload' exact render={renderEmpty} />
         <Route render={renderRedirect} />
