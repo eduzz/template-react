f01facbea13abdbafb5887b2a3b575cb14f232ed
Fernando Moraes <fernando.moraes@eduzz.com>
2020-07-22 15:16:04 -0300

    back commit, sorry

--- src/components/Pages/index.tsx
+++ src/components/Pages/index.tsx
@@ -1,4 +1,3 @@
-import PermissionRoute from 'components/Shared/PermissionRoute';
 import React, { memo, useCallback } from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
@@ -13,7 +12,7 @@ const Pages = memo(() => {
     <BrowserRouter>
       <Switch>
         <Route path='/nova-senha' exact component={NewPasswordPage} />
-        <PermissionRoute path='/' component={AdminPage} />
+        <Route path='/' component={AdminPage} />
 
         <Route path='/reload' exact render={renderEmpty} />
         <Route render={renderRedirect} />
