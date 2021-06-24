50848dbeb0671f170b48036b060998ae6f9d0e8d
Daniel Prado <danielprado.ad@gmail.com>
2021-06-24 10:35:51 -0300

    Watcher
    Signed-off-by: Daniel Prado <danielprado.ad@gmail.com>

---
+++ src/hooks/useWatcher.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { DependencyList } from 'react';
+
+interface IWatchFunction<T> {
+  (callback: (result: T) => void): () => void;
+}
+
+export default function useWatcher<T>(watch: IWatchFunction<T>, deps: DependencyList) {
+  const [data, setData] = useState<T>();
+
+  useEffect(() => {
+    const watcher = watch(result => setData(result));
+    return () => watcher();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps);
+
+  return data;
+}
---
+++ src/services/_mock.ts
@@ -0,0 +1,12 @@
+export default {
+  GET: {},
+  POST: {
+    '/login': {
+      token:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJkYW5pZWwucHJhZG9AZWR1enouY29tIiwiZmlyc3ROYW1lIjoiRGFuaWVsIiwibGFzdE5hbWUiOiJQcmFkbyJ9.MaVt7dCdGyaicE8KPawFKT_Uflrgoz-ivTgcBa34tIU'
+    },
+    '/auth/send-reset': null,
+    '/auth/reset-password': null,
+    '/auth/change-password': null
+  }
+};
