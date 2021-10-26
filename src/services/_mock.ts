5d5ca00100fb890963235b8292b88de742f3343d
Daniel Prado <danielprado.ad@gmail.com>
2021-10-26 11:31:34 -0300

    DIO
    Signed-off-by: Daniel Prado <danielprado.ad@gmail.com>

--- src/services/_mock.ts
+++ src/services/_mock.ts
@@ -5,10 +5,7 @@ const requests = {
       token:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJkYW5pZWwucHJhZG9AZWR1enouY29tIiwiZmlyc3ROYW1lIjoiRGFuaWVsIiwibGFzdE5hbWUiOiJQcmFkbyJ9.MaVt7dCdGyaicE8KPawFKT_Uflrgoz-ivTgcBa34tIU'
     },
-    '/auth/create': {
-      token:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJkYW5pZWwucHJhZG9AZWR1enouY29tIiwiZmlyc3ROYW1lIjoiRGFuaWVsIiwibGFzdE5hbWUiOiJQcmFkbyJ9.MaVt7dCdGyaicE8KPawFKT_Uflrgoz-ivTgcBa34tIU'
-    },
+    '/auth/create': null,
     '/auth/send-reset': null,
     '/auth/reset-password': null,
     '/auth/change-password': null
